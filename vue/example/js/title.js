0d02076a1b25477cfce856e2d4bc7a6ce6c4dff6
wangkun <373658358@qq.com>
2020-07-26 12:11:16 +0800

    vue

--- vue/example/js/title.js
+++ vue/example/js/title.js
@@ -3,20 +3,54 @@
  * title 组件
  */
 
-Vue.component('my-title', {
+// Vue.component('my-title', {
+//     template: `
+//     <div>
+//         <h1>{{text}}</h1>
+//     </div>
+//     `,
+//     props: ['text'],
+//     initData: function() {
+//         return {
+//         }
+//     }
+// })
+
+// 局部注册组件
+var MyBaseInput = {
     template: `
-    <div>
-        <h1>{{text}}</h1>
-    </div>
+        <input
+            v-bind:value="value"
+            v-on:input="$emit('input', $event.target.value)"
+        />
     `,
-    props: ['text'],
+    props: ['value'],
+    mounted: function() {
+        console.log('$attrs>>>', this)
+    },
+}
+
+var MyTitle = {
+    template: `
+    <input
+        class="child-class"
+        style="font-size: 14px;"
+        placeholder="默认placeholder"
+        v-bind:value="text"
+        required
+    />
+    `,
+    inheritAttrs: false,
+    props: ['placeholder', 'text'],
+    created: function() {
+        // const text = this.text;
+        // console.log('type>>>', typeof text);
+    },
+    mounted: function() {
+        console.log('$attrs>>>', this.$attrs)
+    },
     initData: function() {
         return {
         }
     }
-})
-
-// 局部注册组件
-var MyHeader = {
-
 }
